9e4d74fc58443864d7943328966868a6e2814a93
Charlie.Adarsh <charlie.adarsh@rubicotech.in>
2023-11-19 00:59:45 +0530

    code

--- script1.js
+++ script1.js
@@ -1,6 +1,32 @@
 // javascript is a high level object oriented , multi-paradigm programming language.
+
 console.log("hello world");
 let firstname = "Jonas";
 console.log(firstname);
+
 // here firstname is a variable and Jonas is  a  value.
+
 console.log("welcome to Javascript ")
+
+// Don't start variable name with - new, function, uppercase letter, number - 3year, 
+
+let age =23;   // number
+let _name = 'jonas'; //string
+let entry =true; //boolean
+
+let company;// Undefined - value taken by a variable that is not yet defined -empty value
+
+console.log(true);
+console.log(typeof true);
+console.log(typeof entry);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+console.log(typeof _name)
+entry ="yes";
+console.log(typeof entry); //change into string
+// undefined value is value taken by variable that is yet not defined.
+let year ;
+console.log(year);
+console.log(typeof year);
+year= 1997;
+console.log(year);
