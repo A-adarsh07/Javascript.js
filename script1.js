e218805db7541720336c692a0c577ca15ff569d4
Charlie.Adarsh <charlie.adarsh@rubicotech.in>
2023-11-18 22:46:46 +0530

    add code

---
+++ script1.js
@@ -0,0 +1,6 @@
+// javascript is a high level object oriented , multi-paradigm programming language.
+console.log("hello world");
+let firstname = "Jonas";
+console.log(firstname);
+// here firstname is a variable and Jonas is  a  value.
+console.log("welcome to Javascript ")
