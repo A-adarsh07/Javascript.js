ff14eb0eac163eb691afbfcd350ec3faf885b8f4
A-adarsh07 <addy.mishra10@gmail.com>
2024-03-10 02:09:00 +0530

    object formatter

--- script15.js
+++ script15.js
@@ -11,3 +11,33 @@ console.log(obj === obj1) //true
 // OR we can use spread operator 
 obj = {...obj1, ...obj2, ...obj3};
 console.log(obj);
+
+// Array of objects
+const users = [
+    {
+        user1 : "Afreen",
+        email: "afreen@gmail.com",
+        age: 23
+
+    },
+    {
+        user2 : "Xavier",
+        useremail : "xavier@gmail.com",
+        age : 22
+    }
+]
+console.log(users);
+console.log(users[1]);
+
+console.log(Object.keys(users));
+
+console.log(users.hasOwnProperty("email"));  //to find the values or property 
+
+
+// Destructure the objects
+// basically just a syntatically concept
+
+
+
+// json formatter to format Api data 
+// randomuser.me
