7cdff05dd2327850682555511bd2f51b64c78400
A-adarsh07 <addy.mishra10@gmail.com>
2024-03-11 00:55:46 +0530

    Rest operator

---
+++ script12.1.js
@@ -0,0 +1,11 @@
+// Function with Objects and Arrays
+
+// Rest operator
+
+function calculateprice (...num1){
+    return num1
+}
+
+console.log(calculateprice(200,300,4000,5000));
+// the return will create an Array with all the values inside of it. , this is called rest operator.
+
