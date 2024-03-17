22d8c5fa8e27c822fc42306f5628b8ff15fd94de
A-adarsh07 <addy.mishra10@gmail.com>
2024-03-17 20:43:16 +0530

    callstack

--- callstack.js
+++ callstack.js
@@ -6,3 +6,11 @@
 
 // everytime you make function , it'll go through memory phase and execution phase 
 
+// Filters in nodejs   >>> only used with arrays
+const arr =[11,23,3,4,54];
+let result= arr.filter((item)=>{
+return item >11;
+})
+
+console.log(result);
+
