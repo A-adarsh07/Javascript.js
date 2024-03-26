d4d9336266b5ea4911d3499a1a66ef88bae175f4
A-adarsh07 <addy.mishra10@gmail.com>
2024-03-26 17:09:01 +0530

    added something

--- script13.js
+++ script13.js
@@ -93,4 +93,6 @@ console.log(filternum);
 
 const myNumbers=[1,2,3,4,5,6,7,8,9];
 const newNums=myNumbers.map((num) => num*10).map((num)=>num+1).filter((num)=>num >= 30);
+
+
     console.log(newNums);
