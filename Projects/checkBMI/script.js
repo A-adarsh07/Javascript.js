782ca21a698d8f361b70333b228ad4c43b83506b
A-adarsh07 <addy.mishra10@gmail.com>
2024-03-31 01:46:10 +0530

    added js

--- Projects/checkBMI/script.js
+++ Projects/checkBMI/script.js
@@ -11,8 +11,19 @@ form.addEventListener('submit',function(e){
     else if (weight ==='' ||weight<0 || isNaN(weight)){
         results.innerHTML= `Please give a valid weight ${weight}`;
     }else {
-        const bmi = (weight/((height*height)/1000)).toFixed(2);
+        const bmi = (weight/((height*height)/10000)).toFixed(2);
         // show the result 
         results.innerHTML=`<span>${bmi}</span>`;
     }
+    if(results <18.5){
+        console.log("Underweight");
+    }
+   if(results>=18.6 || results <=24.9){
+        console.log("Normal range");
+    }
+    if(results>24.9){
+        console.log("Overweight");
+    }
+
 })
+    
